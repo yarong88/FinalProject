6d2b02fe7ad2ee809989314c30a27d708408c5da
eunji0619 <4484jjj@naver.com>
2023-03-13 16:48:22 +0900

    230313

--- test/src/views/EditProfile.vue
+++ test/src/views/EditProfile.vue
@@ -43,33 +43,37 @@
 
       <div class="hobby-wrap">
         <div class="title">취미</div>
-        <select name="hobby" id="">
+        <select name="hobby" class="selectbox">
           <option v-for="hobby in hobbys" :key="hobby">{{ hobby }}</option>
         </select>
       </div>
-      <button class="profile-save" @click="profile_save">Save</button>
+      <button class="profile-save" @click="profile_save">Modify</button>
     </div>
   </div>
 </template>
 
 <script>
 /* eslint-disable */
+import axios from "axios";
 export default {
   name: "app",
   data() {
     return {
       user: {
-        id: "",
-        email: "",
+        id: localStorage.getItem("login-id"),
+        nickname: "abc",
+        email: "@@@@",
       },
       hobbys: ["운동하기", "노래듣기"],
     };
   },
+
   methods: {
     value() {
-      axios.get("/nickr/" + this.user_id).then((res) => {
-        this.user_nickname = res.data.user_nickname;
-        this.user_email = res.data.user_email;
+      axios.get("/nickr/" + this.user.id).then((res) => {
+        this.user.nickname = res.data.user_nickname;
+        this.user.email = res.data.user_email;
+        this.aa = res.data.user_nickname;
       });
     },
     readURL(input) {
@@ -83,6 +87,10 @@ export default {
         document.getElementsByClassName("profile-edit").src = "";
       }
     },
+  },
+
+  mounted() {
+    this.value();
   },
 };
 </script>
@@ -126,7 +134,7 @@ h3 {
   margin-left: 20px;
 }
 .title {
-  margin-left: 35px;
+  margin-left: 20px;
   text-align: start;
 }
 .editpro_input {
@@ -135,7 +143,7 @@ h3 {
   border-right-width: 0;
   border-top-width: 0;
   border-bottom-width: 1;
-  width: 80%;
+  width: 90%;
   height: 40px;
   margin-bottom: 25px;
 }
@@ -143,8 +151,15 @@ h3 {
   margin-bottom: 20px;
 }
 .profile-save {
+  width: 90%;
+  height: 40px;
   margin-top: 30px;
-  width: 100%;
-  height: 20px;
+  background-color: rgb(163, 163, 163);
+  cursor: pointer;
+}
+.selectbox {
+  width: 200px;
+  text-align: center;
+  height: 30px;
 }
 </style>
